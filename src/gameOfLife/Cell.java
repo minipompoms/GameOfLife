7f706afeafbbe03021e4942a360138f156a1883a
Paige Kohn <paigkay@gmail.com>
2018-11-05 09:11:48 -0500

    revisions to include Chana's notes

--- src/gameOfLife/Cell.java
+++ src/gameOfLife/Cell.java
@@ -1,11 +1,14 @@
 package gameOfLife;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Cell {
 
     private Point point;
     private boolean isAlive;
+    ArrayList<Cell> neighbors = new ArrayList<>();
+
 
     public Cell(Point point){
         this.point = point;
@@ -19,4 +22,47 @@ public class Cell {
     public boolean isAlive() {
         return isAlive;
     }
-}
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+
+    public ArrayList<Cell> setAllNeighbors(Cell cell) {
+        Point point = new Point();
+        int x = cell.getPoint().x;
+        int y = cell.getPoint().y;
+        point.setLocation(x-1,y-1 );
+        cell.setPoint(point);
+        neighbors.add(cell);
+        point.setLocation(x-1, y);
+        cell.setPoint(point);
+        neighbors.add(cell);
+        point.setLocation(x-1, y+1);
+        cell.setPoint(point);
+        neighbors.add(cell);
+        point.setLocation(x-1, y+1);
+        cell.setPoint(point);
+        neighbors.add(cell);
+        point.setLocation(x, y-1);
+        cell.setPoint(point);
+        neighbors.add(cell);
+        point.setLocation(x, y+1);
+        cell.setPoint(point);
+        neighbors.add(cell);
+        point.setLocation(x+1, y-1);
+        cell.setPoint(point);
+        neighbors.add(cell);
+        point.setLocation(x+1, y);
+        cell.setPoint(point);
+        neighbors.add(cell);
+        point.setLocation(x+1, y+1);
+        cell.setPoint(point);
+        neighbors.add(cell);
+        return neighbors;
+    }
+
+    }
--- src/gameOfLife/Grid.java
+++ src/gameOfLife/Grid.java
@@ -2,31 +2,49 @@ package gameOfLife;
 
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Grid {
 
-    private int rows;
-    private int columns;
-    private Cell[][] grid;
+    private final int rows;
+    private final int columns;
+    private Cell [][] grid;
+    private final Random RANDOM = new Random();
+    private ArrayList<Cell> aliveNeighbors;
 
     public Grid(int rows, int columns){
         this.rows = rows;
         this.columns = columns;
-        this.grid = new Cell[rows][columns];
+        this.grid = new Cell[this.rows][this.columns];
     }
 
-    public void setGrid(){
-        int x = 0;
-        int y = 0;
-        Point point = new Point(x,y);
-
+    public void mapGrid(){
+        boolean cellState = RANDOM.nextBoolean();
+        int x;
+        int y;
         for(x = 0; x < rows; x++){
             for (y = 0; y < columns; y++){
-                Cell cell = new Cell(point)  ;
+                Point point = new Point(x,y);
+                grid[x][y] = new Cell(point);
+                grid[x][y].setAlive(cellState);
             }
         }
     }
 
+    public Cell getCell(){
+        return grid[rows][columns];
+    }
 
-
+    public ArrayList<Cell> setAliveNeighbors(Cell cell){
+        aliveNeighbors = new ArrayList<>();
+        int size = cell.setAllNeighbors(cell).size();
+        for(int i = 0; i < size; i++) {
+            Cell neighbor = cell.neighbors.get(i);
+            if(neighbor.isAlive()){
+                aliveNeighbors.add(neighbor);
+            }
+        }
+      return aliveNeighbors;
+    }
 }
