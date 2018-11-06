36980f79fc52ae77e8754b7f29f3d5ccb7042b0f
cdruk <cdrukier@student.touro.edu>
2018-11-05 23:32:19 -0500

    started algorithm and some questions

---
+++ src/gameOfLife/Algorithm.java
@@ -0,0 +1,40 @@
+package gameOfLife;
+
+
+public class Algorithm {
+//    Any live cell with fewer than two live neighbors dies, as if caused by under population.
+//    Any live cell with two or three live neighbors lives on to the next generation.
+//    Any live cell with more than three live neighbors dies, as if by overpopulation.
+//    Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+    private Grid grid;
+
+    public Algorithm(Grid grid) {
+        this.grid = grid;
+
+        nextGen(grid);
+    }
+
+    private void nextGen(Grid grid) {
+        int liveNeighbors;
+        for (Cell[] cellRow : grid.getGrid()) {
+            for (Cell cell : cellRow) {
+                liveNeighbors = cell.getAliveNeighbors().size();
+                switch (liveNeighbors){
+                    case (2):
+                        break;
+                    case (3):
+                        cell.setAlive(true);
+                        break;
+                    default:
+                        cell.setAlive(false);
+                        break;
+                }
+
+            }
+
+        }
+
+
+    }
+}
--- src/gameOfLife/Grid.java
+++ src/gameOfLife/Grid.java
@@ -9,42 +9,83 @@ public class Grid {
 
     private final int rows;
     private final int columns;
-    private Cell [][] grid;
+    private Cell[][] grid;
     private final Random RANDOM = new Random();
     private ArrayList<Cell> aliveNeighbors;
+    private Algorithm algorithm;
 
-    public Grid(int rows, int columns){
+    public Grid(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
         this.grid = new Cell[this.rows][this.columns];
+
+        //call in the constructor to create
+        mapGrid();
+        setupNeighbors();
     }
 
-    public void mapGrid(){
+    private void setupNeighbors() {
+        for (Cell[] cellRow : grid) {
+            for (Cell cell : cellRow) {
+                cell.setAllNeighbors(cell);
+            }
+
+        }
+    }
+
+
+    public void mapGrid() {
         boolean cellState = RANDOM.nextBoolean();
         int x;
         int y;
-        for(x = 0; x < rows; x++){
-            for (y = 0; y < columns; y++){
-                Point point = new Point(x,y);
+        for (x = 0; x < rows; x++) {
+            for (y = 0; y < columns; y++) {
+                Point point = new Point(x, y);
                 grid[x][y] = new Cell(point);
                 grid[x][y].setAlive(cellState);
             }
         }
     }
 
-    public Cell getCell(){
+    void getNextGen() {
+        algorithm = new Algorithm(this);
+    }
+
+    public Cell getCell() {
         return grid[rows][columns];
     }
 
-    public ArrayList<Cell> setAliveNeighbors(Cell cell){
-        aliveNeighbors = new ArrayList<>();
-        int size = cell.setAllNeighbors(cell).size();
-        for(int i = 0; i < size; i++) {
-            Cell neighbor = cell.neighbors.get(i);
-            if(neighbor.isAlive()){
-                aliveNeighbors.add(neighbor);
+    public Cell[][] getGrid() {
+        return grid;
+    }
+
+    //shouldn't Cell contain it's neighbors?
+//    public ArrayList<Cell> setAliveNeighbors(Cell cell){
+//        aliveNeighbors = new ArrayList<>();
+//        int size = cell.setAllNeighbors(cell).size();
+//        for(int i = 0; i < size; i++) {
+//            Cell neighbor = cell.neighbors.get(i);
+//            if(neighbor.isAlive()){
+//                aliveNeighbors.add(neighbor);
+//            }
+//        }
+//      return aliveNeighbors;
+//    }
+
+    @Override
+    public String toString() {
+        StringBuilder gridPrint = new StringBuilder();
+        for (Cell[] cellRow : grid) {
+            for (Cell cell : cellRow) {
+                if (cell.isAlive()) {
+                    gridPrint.append("*");
+                } else {
+                    gridPrint.append("-");
+                }
             }
+            gridPrint.append("\n");
         }
-      return aliveNeighbors;
+        return gridPrint.toString();
+
     }
 }
